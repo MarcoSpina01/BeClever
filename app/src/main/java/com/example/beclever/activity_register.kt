afc4fb6bc0d76efcd93787acb4068a667f5029cf
Paccise <mazzada2001@gmail.com>
2023-05-04 17:50:10 +0200

    registrazione

--- app/src/main/java/com/example/beclever/activity_register.kt
+++ app/src/main/java/com/example/beclever/activity_register.kt
@@ -1,11 +1,23 @@
 package com.example.beclever
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.beclever.ui.login.LoginActivity
 
 class activity_register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val backToLogin = findViewById<TextView>(R.id.BackToLogin)
+        backToLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
+
+
 }
