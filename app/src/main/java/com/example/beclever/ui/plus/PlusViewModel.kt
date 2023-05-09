2f227eb317e4abcc2b541f5df8188806df8d3b34
MarcoSpina01 <marco.spina01@icloud.com>
2023-05-09 18:00:40 +0200

    update

--- app/src/main/java/com/example/beclever/ui/plus/PlusViewModel.kt
+++ app/src/main/java/com/example/beclever/ui/plus/PlusViewModel.kt
@@ -1,4 +1,13 @@
 package com.example.beclever.ui.plus
 
-class PlusViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class PlusViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is settings Fragment"
+    }
+    val text: LiveData<String> = _text
 }
