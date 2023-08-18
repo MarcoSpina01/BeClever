d52296337684ecd3cb3d7a0666ce61a4b32d2be1
MarcoSpina01 <marco.spina01@icloud.com>
2023-08-18 12:38:19 +0200

    update

--- app/src/main/java/com/example/beclever/ui/profile/User.kt
+++ app/src/main/java/com/example/beclever/ui/profile/User.kt
@@ -7,4 +7,12 @@ import com.google.firebase.firestore.FirebaseFirestore
 data class User(
     private var nome: String,
     private var email: String
-)
+) {
+    fun getNome(): String {
+        return nome
+    }
+
+    fun getEmail(): String {
+        return email
+    }
+}
