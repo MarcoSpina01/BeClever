f80b63d9cecee9cd4baf85ff4c08fc161f642582
MarcoSpina01 <marco.spina01@icloud.com>
2023-09-07 12:12:57 +0200

    Commenti

--- app/src/main/java/com/example/beclever/ui/notifications/NotificationModel.kt
+++ app/src/main/java/com/example/beclever/ui/notifications/NotificationModel.kt
@@ -4,6 +4,16 @@ import java.io.Serializable
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
+/**
+ * Modello di dati per rappresentare una notifica.
+ *
+ * @property message Il testo della notifica.
+ * @property date La data della notifica in formato numerico lungo (ad esempio, 20230907 per il 7 settembre 2023).
+ * @property time L'orario della notifica in formato numerico lungo (ad esempio, 154532 per le 15:45:32).
+ * @property userId L'ID dell'utente a cui è destinata la notifica.
+ * @property lessonId L'ID della lezione associata alla notifica.
+ * @property clientId L'ID del cliente associato alla notifica.
+ */
 data class NotificationModel(
 
     var message: String,
@@ -13,10 +23,17 @@ data class NotificationModel(
     var lessonId: String?,
     var clientId: String?
 
-) : Serializable
-{
+) : Serializable {
+    /**
+     * Costruttore primario con parametri opzionali inizializzati a valori predefiniti vuoti.
+     */
     constructor() : this("", null, null, "", "", "")
 
+    /**
+     * Calcola il tempo trascorso tra la notifica e il momento attuale in una forma leggibile.
+     *
+     * @return Una stringa che rappresenta il tempo trascorso dalla notifica (ad esempio, "2 ore fa").
+     */
     fun getTempoPassato(): String {
         val current = LocalDateTime.now()
         val formatter = DateTimeFormatter.BASIC_ISO_DATE
@@ -41,7 +58,6 @@ data class NotificationModel(
                     return "${(currentTime - time!!)} secondi fa"
                 }
 
-
                 return ""
 
             } else {
@@ -54,9 +70,8 @@ data class NotificationModel(
                 } else {
                     return "${(currentDate/10000 - date!!/10000)} anni fa"
                 }
-
             }
         }
-        else return ""
+        return ""
     }
 }
--- app/src/main/java/com/example/beclever/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/beclever/ui/notifications/NotificationsFragment.kt
@@ -10,12 +10,21 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.beclever.databinding.FragmentNotificationsBinding
 
+/**
+ * Fragment per la visualizzazione delle notifiche dell'utente.
+ */
 class NotificationsFragment : Fragment() {
 
+    // ViewModel per la gestione delle notifiche
     private lateinit var notificationViewModel: NotificationsViewModel
+
+    // Adapter per la visualizzazione delle notifiche
     private lateinit var notificationsAdapter: NotificationsAdapter
+
+    // Binding per l'uso di View Binding
     private var _binding: FragmentNotificationsBinding? = null
 
+    // Proprietà di accesso per il binding
     private val bindingView get() = _binding!!
 
     override fun onCreateView(
@@ -27,19 +36,26 @@ class NotificationsFragment : Fragment() {
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = bindingView.root
 
+        // Inizializza il ViewModel per le notifiche
         notificationViewModel = ViewModelProvider(this)[NotificationsViewModel::class.java]
+
+        // Collega il ViewModel al layout tramite View Binding
         bindingView.viewModel = notificationViewModel
         bindingView.lifecycleOwner = viewLifecycleOwner
 
+        // Inizializza il RecyclerView per visualizzare le notifiche
         val recyclerView: RecyclerView = bindingView.recyclerViewNotifications
 
+        // Osserva le modifiche nella lista delle notifiche nel ViewModel
         notificationViewModel.notificationsLiveData.observe(viewLifecycleOwner) { notifications ->
             // Aggiorna l'adapter con le nuove notifiche
             notificationsAdapter.updateData(notifications)
         }
 
+        // Ottieni le notifiche passate come argomento al fragment (se presenti)
         val notificationModels = arguments?.getSerializable("NotificationsList") as? List<NotificationModel>
 
+        // Inizializza l'adapter con le notifiche ottenute dagli argomenti o una lista vuota se non ci sono argomenti
         notificationsAdapter = NotificationsAdapter(notificationModels ?: emptyList())
         recyclerView.adapter = notificationsAdapter
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
@@ -50,20 +66,23 @@ class NotificationsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Inizializza nuovamente il ViewModel (non è necessario, duplicato)
         notificationViewModel = ViewModelProvider(this)[NotificationsViewModel::class.java]
         bindingView.viewModel = notificationViewModel
         bindingView.lifecycleOwner = viewLifecycleOwner
 
+        // Ottieni il RecyclerView per visualizzare le notifiche
         val recyclerView: RecyclerView = bindingView.recyclerViewNotifications
 
+        // Ottieni le notifiche passate come argomento al fragment (se presenti)
         val notificationModels = arguments?.getSerializable("NotificationsList") as? List<NotificationModel>
         if (notificationModels != null) {
-            // Inizializza l'adapter qui
+            // Inizializza l'adapter qui con le notifiche ottenute dagli argomenti
             notificationsAdapter = NotificationsAdapter(notificationModels)
             recyclerView.adapter = notificationsAdapter
             recyclerView.layoutManager = LinearLayoutManager(requireContext())
         } else {
-            // La conversione non è riuscita o l'argomento non è presente
+            // Gestisci il caso in cui la conversione non è riuscita o l'argomento non è presente
         }
     }
 
